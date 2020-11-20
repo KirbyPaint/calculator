5671a6b640b27d3fd1b5b7feca687bb190f2d679
KirbyPaint <porter.ashley13@gmail.com>
2020-11-19 19:54:16 -0800

    add functionality to all buttons. Bug where button does not perform click until a second button is clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -13,13 +13,47 @@ function multiply(number1, number2) {
 function divide(number1, number2) {
   return number1 / number2;
 }
-
+/*
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
+  $("form#calculate").submit(function(event) {
+   event.preventDefault();
+    const number1 = parseInt($("#calculate1").val());
+    const number2 = parseInt($("#calculate2").val());
     const result = add(number1, number2);
     $("#output").text(result);
+  });
+});
+*/
+// Works now for all functions but one button must be pressed before it will work
+
+$(document).ready(function() {
+  $("form#calculate").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#calculate1").val());
+    const number2 = parseInt($("#calculate2").val());
+
+    $("#add").unbind().click(function() {
+      const result = add(number1, number2);
+      $("#output").text(result);
+      console.log(result);
+    });
+
+    $("#sub").unbind().click(function() {
+      const result = subtract(number1, number2);
+      $("#output").text(result);
+      console.log(result);
+    });
+
+    $("#mul").unbind().click(function() {
+      const result = multiply(number1, number2);
+      $("#output").text(result);
+      console.log(result);
+    });
+
+    $("#div").unbind().click(function() {
+      const result = divide(number1, number2);
+      $("#output").text(result);
+      console.log(result);
+    });
   });
 });
