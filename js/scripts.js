af0cb8a9c17c005888e8b583d19ef9eb4c8849a4
KirbyPaint <porter.ashley13@gmail.com>
2020-11-19 17:54:17 -0800

    add better user interface, back to the original working calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,24 @@
-// Business logic:
-
-const add = function(number1, number2) {
+function add(number1, number2) {
   return number1 + number2;
-};
+}
 
-const subtract = function(number1, number2) {
+function subtract(number1, number2) {
   return number1 - number2;
-};
+}
 
-const multiply = function(number1, number2) {
+function multiply(number1, number2) {
   return number1 * number2;
 };
 
-const divide = function(number1, number2) {
+function divide(number1, number2) {
   return number1 / number2;
 };
 
-// Everything below this line is user interface logic:
-
 $(document).ready(function() {
-  const number1 = parseInt(prompt("Enter a number:"));
-  const number2 = parseInt(prompt("Enter another number:"));
-
-  alert(add(number1, number2));
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    alert(add(number1, number2));
+  });
 });
