fe2129033440044daae3c54262417d658f018528
KirbyPaint <porter.ashley13@gmail.com>
2020-11-19 20:22:06 -0800

    commit final code to be worked on later

--- js/scripts.js
+++ js/scripts.js
@@ -15,35 +15,32 @@ function divide(number1, number2) {
 }
 
 // Works now for all functions but one button must be pressed before it will work
-
 $(document).ready(function() {
-  $("form#calculate").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#calculate1").val());
-    const number2 = parseInt($("#calculate2").val());
+  $(document).ready(function() {
+    $("form#calculate").submit(function(event) {
+      event.preventDefault();
+      const number1 = parseInt($("#calculate1").val());
+      const number2 = parseInt($("#calculate2").val());
 
-    $("#add").unbind().click(function() { // Adding unbind() prevents it from returning exponentially.
-      const result = add(number1, number2);
-      $("#output").text(result);
-      console.log(result);
-    });
+      $("#add").unbind().click(function() { // Adding unbind() prevents it from returning exponentially.
+        const result = add(number1, number2);
+        $("#output").text(result);
+      });
 
-    $("#sub").unbind().click(function() {
-      const result = subtract(number1, number2);
-      $("#output").text(result);
-      console.log(result);
-    });
+      $("#sub").unbind().click(function() {
+        const result = subtract(number1, number2);
+        $("#output").text(result);
+      });
 
-    $("#mul").unbind().click(function() {
-      const result = multiply(number1, number2);
-      $("#output").text(result);
-      console.log(result);
-    });
+      $("#mul").unbind().click(function() {
+        const result = multiply(number1, number2);
+        $("#output").text(result);
+      });
 
-    $("#div").unbind().click(function() {
-      const result = divide(number1, number2);
-      $("#output").text(result);
-      console.log(result);
+      $("#div").unbind().click(function() {
+        const result = divide(number1, number2);
+        $("#output").text(result);
+      });
     });
   });
 });
