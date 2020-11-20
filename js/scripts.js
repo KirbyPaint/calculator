1f08bb9b5b8762ef76c51bcd0430086d7cb7b0d4
KirbyPaint <porter.ashley13@gmail.com>
2020-11-19 17:57:35 -0800

    add calc result to page instead of alert, like before

--- js/scripts.js
+++ js/scripts.js
@@ -8,17 +8,18 @@ function subtract(number1, number2) {
 
 function multiply(number1, number2) {
   return number1 * number2;
-};
+}
 
 function divide(number1, number2) {
   return number1 / number2;
-};
+}
 
 $(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
-    alert(add(number1, number2));
+    const result = add(number1, number2);
+    $("#output").text(result);
   });
 });
